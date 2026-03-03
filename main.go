58831adb536e4173cdb37548ca4ac0fe813b08b3
Melih Genel <mlihgenel@gmail.com>
2026-03-03 16:11:39 +0300

    refactor: share application entrypoint

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"os"
-	"runtime/debug"
-	"strings"
 
-	"github.com/mlihgenel/fileconverter-cli/cmd"
+	"github.com/mlihgenel/fileconverter-cli/internal/entrypoint"
 )
 
 var (
@@ -16,63 +14,5 @@ var (
 )
 
 func main() {
-	cmd.SetVersionInfo(resolveVersion(), resolveBuildDate())
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
-	}
-}
-
-func resolveVersion() string {
-	v := normalizeVersion(version)
-	if v != "" && v != "dev" {
-		return v
-	}
-
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return "dev"
-	}
-	if moduleVersion := normalizeVersion(info.Main.Version); moduleVersion != "" && moduleVersion != "(devel)" {
-		return moduleVersion
-	}
-
-	revision := strings.TrimSpace(buildSetting(info, "vcs.revision"))
-	if revision == "" {
-		return "dev"
-	}
-	if len(revision) > 7 {
-		revision = revision[:7]
-	}
-	if strings.EqualFold(strings.TrimSpace(buildSetting(info, "vcs.modified")), "true") {
-		return "dev-" + revision + "-dirty"
-	}
-	return "dev-" + revision
-}
-
-func resolveBuildDate() string {
-	if v := strings.TrimSpace(buildDate); v != "" {
-		return v
-	}
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return ""
-	}
-	return strings.TrimSpace(buildSetting(info, "vcs.time"))
-}
-
-func buildSetting(info *debug.BuildInfo, key string) string {
-	for _, setting := range info.Settings {
-		if setting.Key == key {
-			return setting.Value
-		}
-	}
-	return ""
-}
-
-func normalizeVersion(v string) string {
-	v = strings.TrimSpace(v)
-	if strings.HasPrefix(v, "v") {
-		return strings.TrimPrefix(v, "v")
-	}
-	return v
+	os.Exit(entrypoint.Run(version, buildDate))
 }
